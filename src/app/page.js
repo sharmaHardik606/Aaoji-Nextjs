48f653287f877c33ccab51e22efc5857b3de8cc6
Hardik Sharma <sharmahardik606@gmail.com>
2025-04-13 15:15:28 +0530

    Minor Error Fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar";
-import Hero from "@/sections/Hero";
 import Features from "@/sections/Features";
 import About from "@/sections/About";
 import Gallery from "@/sections/Gallery";
